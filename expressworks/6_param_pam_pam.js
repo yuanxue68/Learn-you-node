91e9df79215e55d42aaebca66ffcfa42c1f83e55
Yuan Xue <yuanxue68@gmail.com>
2015-03-29 03:06:12 -0400

    ex6

---
+++ expressworks/6_param_pam_pam.js
@@ -0,0 +1,11 @@
+var express=require('express');
+var crypto=require('crypto');
+
+var app=express();
+app.put('/message/:id',function(req,res){
+	res.send(
+		crypto.createHash('sha1').update(new Date().toDateString()+
+			req.params.id).digest('hex')
+		)
+})
+app.listen(process.argv[2]);
